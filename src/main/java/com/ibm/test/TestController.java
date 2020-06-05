3ef3a57d28fafdb98418522635d081f03dea35af
Pietro94 <pietro.gerace@ibm.com>
2020-06-05 17:51:26 +0200

    aggiunto codice per ottenre il JWT token

--- src/main/java/com/ibm/test/TestController.java
+++ src/main/java/com/ibm/test/TestController.java
@@ -1,8 +1,10 @@
 package com.ibm.test;
 
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.representations.AccessToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 import net.sf.json.JSONObject;
@@ -15,8 +17,12 @@ public class TestController {
     public String helloWorld(Principal principal){
         Authentication authentication = (Authentication) principal;
         JSONObject object = JSONObject.fromObject(authentication);
-        System.out.println(object);
-        return "Hello World from Keycloak!";
+
+        Authentication authenticationToken = SecurityContextHolder.getContext().getAuthentication();
+        Object detailsToken = authenticationToken.getDetails();
+        SimpleKeycloakAccount account = (SimpleKeycloakAccount) detailsToken;
+        String token = account.getKeycloakSecurityContext().getTokenString();
+        return "Hello World from Keycloak! Token : " + token;
     }
 
 }
